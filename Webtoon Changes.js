4801f4df6d02c596000d55f1b076ed2e075ba483
Chris Britt <cab.britt@gmail.com>
2020-03-02 19:42:11 -0500

    Update Webtoon Changes.js

--- Webtoon Changes.js
+++ Webtoon Changes.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sukkubix's Webtoon Changes
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Script to allow easy navigation of webtoons by clicking in the margin
 // @author       Sukkubix
 // @match        https://www.webtoons.com/*/viewer*
@@ -13,7 +13,7 @@
     'use strict';
 
     // Your code here...
-    $("#_viewerBox").on("click", function(){
+    $("#_viewerBox").parent().on("click", function(){
         let percentThreshold = 0.25;
         let nextThreshold = (1-percentThreshold)*$(document).width();
         let prevThreshold = percentThreshold*$(document).width();
